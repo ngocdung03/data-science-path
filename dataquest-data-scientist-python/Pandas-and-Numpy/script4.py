8e4ccbd37336b4c628ff9fa454c68084388b5c52
ngocdung03 <dungdidom@gmail.com>
2021-03-14 18:27:33 +0900

    Update

---
+++ dataquest-data-scientist-python/Pandas-and-Numpy/script4.py
@@ -0,0 +1,11 @@
+## Pandas Intermediate
+import pandas as pd
+# read the data set into a pandas dataframe
+f500 = pd.read_csv("f500.csv", index_col=0)
+f500.index.name = None
+
+# replace 0 values in the "previous_rank" column with NaN
+f500.loc[f500["previous_rank"] == 0, "previous_rank"] = np.nan
+
+# Select columns
+f500_selection = f500.loc[:, ["rank", "revenues", "revenue_change"]].head(5)
