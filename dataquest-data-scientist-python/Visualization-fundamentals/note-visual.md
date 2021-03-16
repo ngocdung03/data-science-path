754edd63be313e5e0115a7856a59fbb142391b2b
ngocdung03 <dungdidom@gmail.com>
2021-03-16 17:46:06 +0900

    Finish line graph

--- dataquest-data-scientist-python/Visualization-fundamentals/note-visual.md
+++ dataquest-data-scientist-python/Visualization-fundamentals/note-visual.md
@@ -8,3 +8,16 @@ plt.plot(month_number, new_cases)
 plt.ticklabel_format(axis='y', style='plain')
 plt.show()
 ```
+-  We call a series of data points that is listed in time order a time series
+- [Three types of growth.jpg]
+- Drawing 2 line graphs with the same axes: 
+```python
+plt.plot(france['Date_reported'], france['Cumulative_cases'],
+         label='France')
+plt.plot(uk['Date_reported'], uk['Cumulative_cases'],
+         label='The UK')
+plt.legend()
+plt.show()
+#When we use plt.plot() the first time, Matplotlib creates a line graph. When we use plt.plot() again, Matplotlib creates another line graph that shares the same x- and y-axis as the first graph. If we want Matplotlib to draw the second line graph separately, we need to close the first graph with the plt.show() function.
+```
+- 
--- dataquest-data-scientist-python/Visualization-fundamentals/script.py
+++ dataquest-data-scientist-python/Visualization-fundamentals/script.py
@@ -4,4 +4,46 @@ month_number = [1, 2, 3, 4, 5, 6, 7]
 new_deaths = [213, 2729, 37718, 184064, 143119, 136073, 165003]
 import matplotlib.pyplot as plt
 plt.plot(month_number, new_deaths)
+plt.title('New Reported Deaths By Month (Globally)')
+plt.xlabel('Month Number')
+plt.ylabel('Number Of Deaths')
 plt.show()
+
+import pandas as pd
+who_time_series = pd.read_csv("WHO_time_series.csv")
+# Transform to datetime data type
+who_time_series["Date_reported"] = pd.to_datetime(who_time_series["Date_reported"])
+who_time_series.head(5)
+who_time_series.tail(5)
+who_time_series.info()
+
+# Plot
+def plot_cumulative_cases(country_name):
+    country = who_time_series[who_time_series['Country'] == country_name]
+    plt.plot(country['Date_reported'], country['Cumulative_cases'])
+    plt.title('{}: Cumulative Reported Cases'.format(country_name))
+    plt.xlabel('Date')
+    plt.ylabel('Number of Cases')
+    plt.show()
+    
+plot_cumulative_cases('Brazil')
+plot_cumulative_cases('Iceland')
+plot_cumulative_cases('Argentina')
+
+brazil = 'exponential'
+iceland = 'logarithmic'
+argentina = 'exponential'
+
+# Plot the evolution of cumulative cases for France, the United Kingdom, and Italy on the same graph.
+france = who_time_series[who_time_series['Country'] == 'France']
+uk = who_time_series[who_time_series['Country'] == 'The United Kingdom']
+italy = who_time_series[who_time_series['Country'] == 'Italy']
+plt.plot(france['Date_reported'], france['Cumulative_cases'], label='France')
+plt.plot(uk['Date_reported'], uk['Cumulative_cases'], label='The UK')
+plt.plot(italy['Date_reported'], italy['Cumulative_cases'], label='Italy')
+plt.legend()
+plt.show()
+greatest_july = 'UK'
+lowest_july = 'France'
+increase_march = 'Italy'
+
