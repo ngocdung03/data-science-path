07f8b99a5a0ef4c07c16b82f8862c56a2b76a369
ngocdung03 <dungdidom@gmail.com>
2021-03-19 19:34:39 +0900

    Initialize Combining data

--- dataquest-data-scientist-python/Cleaning-and-analysis/note-cleaning-analysis.md
+++ dataquest-data-scientist-python/Cleaning-and-analysis/note-cleaning-analysis.md
@@ -11,4 +11,13 @@ happiness2015.pivot_table(values='Happiness Score', index='Region', aggfunc=np.m
 # may omit aggfunc because mean is the default aggregation function of df.pivot_table()
 ```
 
-
+##### Combining data with Pandas
+- pd.concat([df1, df2]):
+    - Stacked: axis=0 (default)
+    - Side by side: axis=1
+    - ignore_index: reset the index
+    - By default, the function will keep ALL of the data, no matter if missing values are created.
+- pd.merge(): execute high performance database-style joins: `pd.merge(left=df1, right=df2, on='Col_Name')     #on=col used as the key`
+    - much more common to use inner and left joins for database-style joins
+    - Inner join by index: `pd.merge(left=four_2015, right=three_2016, left_index=True, right_index=True, suffixes=('_2015','_2016'))`
+- [pd.concat-vs-pd.merge.jpg] 
---
+++ dataquest-data-scientist-python/Cleaning-and-analysis/script2.py
@@ -0,0 +1,25 @@
+## Combining data with Pandas
+import pandas as pd
+happiness2015 = pd.read_csv("World_Happiness_2015.csv")
+happiness2016 = pd.read_csv("World_Happiness_2016.csv")
+happiness2017 = pd.read_csv("World_Happiness_2017.csv")
+
+happiness2015['Year'] = 2015
+happiness2016['Year'] = 2016
+happiness2017['Year'] = 2017
+
+# pd.merge()
+three_2015 = happiness2015[['Country','Happiness Rank','Year']].iloc[2:5]
+three_2016 = happiness2016[['Country','Happiness Rank','Year']].iloc[2:5]
+merged = pd.merge(left=three_2015, right=three_2016, on='Country')
+merged_left = pd.merge(left=three_2015, right=three_2016, on='Country', how='left')
+merged_left_updated = pd.merge(left=three_2016, right=three_2015, on='Country', how='left')
+
+merged_suffixes = pd.merge(left=three_2015, right=three_2016, how = 'left', on='Country', suffixes=('_2015', '_2016'))
+merged_updated_suffixes = pd.merge(left=three_2016, right=three_2015, how = 'left', on='Country', suffixes=('_2016', '_2015'))
+
+# Merge by index instead of key
+merge_index = pd.merge(left = four_2015,right = three_2016, left_index = True, right_index = True, suffixes = ('_2015','_2016'))
+merge_index_left = pd.merge(left = four_2015,right = three_2016, left_index = True, right_index = True, suffixes = ('_2015','_2016'), how='left')
+
+# Comparing world happiness from 2015 to 2017
