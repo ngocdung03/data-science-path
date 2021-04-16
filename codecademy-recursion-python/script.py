9b713e0a417b064bd3541790ccc80cd62b23c81d
ngocdung03 <dungdidom@gmail.com>
2021-04-16 19:15:23 +0900

    recursive function

--- codecademy-recursion-python/script.py
+++ codecademy-recursion-python/script.py
@@ -74,3 +74,28 @@ def flatten(my_list):
   return result
 
 flatten(planets)
+
+## Binary search tree: two children at most per tree node, one < parent and the other is greater.
+
+def build_bst(my_list):  # sorted list of values as input.
+  # base case
+  if len(my_list)==0: 
+    return "No Child"
+  # recursive step
+  middle_idx = len(my_list)//2 #round(len(my_list)/2)
+  middle_value = my_list[middle_idx]
+  print("Middle index: {0}".format(middle_idx))
+  print("Middle value: {0}".format(middle_value))
+  tree_node = {"data":middle_value}
+  tree_node["left_child"] = build_bst(my_list[:middle_idx])
+  tree_node["right_child"] = build_bst(my_list[middle_idx+1:])
+  return tree_node
+
+# For testing
+sorted_list = [12, 13, 14, 15, 16]
+binary_search_tree = build_bst(sorted_list)
+print(binary_search_tree)
+
+# fill in the runtime as a string
+# 1, logN, N, N*logN, N^2, 2^N, N!
+runtime = "N*logN"
