4f03351f1aaecf0270d5628070f2ed39a75f0a9f
ngocdung <dungdidom@gmail.com>
2022-01-12 19:27:06 +0900

    Magic methods

--- Python OOP/oop.py
+++ Python OOP/oop.py
@@ -299,3 +299,202 @@ p4.display()
 
 e1 = Employee('James', 'Smith', 1990, 6000)
 p5 = Person.from_employee(e1)
+
+# Static method 
+
+# Magic method
+# Magic Methods - 1
+class Fraction:
+    def __init__(self,nr,dr=1):
+        self.nr = nr
+        self.dr = dr
+        if self.dr < 0:
+            self.nr *= -1
+            self.dr *= -1
+        self._reduce()
+
+    def show(self):
+        print(f'{self.nr}/{self.dr}')
+
+    def __add__(self,other):
+        if isinstance(other,int):
+            other = Fraction(other)
+        f = Fraction(self.nr * other.dr + other.nr * self.dr,
+    self.dr * other.dr)
+        f._reduce()
+        return f
+
+    def __sub__(self,other):   #subtract
+        if isinstance(other,int):
+            other = Fraction(other)
+        f = Fraction(self.nr * other.dr - other.nr * self.dr,
+    self.dr * other.dr)
+        f._reduce()
+        return f
+
+    def __mul__(self,other):
+        if isinstance(other,int):
+            other = Fraction(other)
+        f = Fraction(self.nr * other.nr , self.dr * other.dr)
+        f._reduce()
+        return f
+
+    def _reduce(self):
+        h = Fraction.hcf(self.nr, self.dr)
+        if h == 0:
+            return
+        self.nr //= h
+        self.dr //= h
+
+    @staticmethod
+    def hcf(x,y):
+        x=abs(x)
+        y=abs(y)
+        smaller = y if x>y else x
+        s = smaller
+        while s>0:
+            if x%s==0 and y%s==0:
+                break
+            s-=1
+        return s
+>>>f1 = Fraction(2,3)
+>>>f2 = Fraction(3,4)
+>>>f3 = f1+f2
+>>>f3 = f1*f2
+>>>f3 = f1.add(f2)
+>>>f3.show()
+>>>f3 = f1.multiply(f2)
+
+class Fraction:
+    def __init__(self,nr,dr=1):
+        self.nr = nr
+        self.dr = dr
+        if self.dr < 0:
+            self.nr *= -1
+            self.dr *= -1
+        self._reduce()
+    def show(self):
+        print(f'{self.nr}/{self.dr}')
+    def __add__(self,other):
+        if isinstance(other,int):
+            other = Fraction(other)
+        f = Fraction(self.nr * other.dr + other.nr * self.dr, self.dr * other.dr)
+        f._reduce()
+        return f
+
+    def __sub__(self,other):
+        if isinstance(other,int):
+            other = Fraction(other)
+        f = Fraction(self.nr * other.dr - other.nr * self.dr, self.dr * other.dr)
+        f._reduce()
+        return f
+    def __mul__(self,other):
+        if isinstance(other,int):
+            other = Fraction(other)
+        f = Fraction(self.nr * other.nr , self.dr * other.dr)
+        f._reduce()
+        return f
+    def _reduce(self):
+        h = Fraction.hcf(self.nr, self.dr)
+        if h == 0:
+            return
+        self.nr //= h
+        self.dr //= h
+    @staticmethod
+    def hcf(x,y):
+        x=abs(x)
+        y=abs(y)
+        smaller = y if x>y else x
+        s = smaller
+        while s>0:
+            if x%s==0 and y%s==0:
+                break
+            s-=1
+        return s
+>>>f3 = f1.__add__(f2)
+>>>f3
+>>>f3 = f1 + f2
+>>>f3
+>>>f3 = f1-f2
+>>>f3
+>>>f3 = f1*f2"Python in Depth" by Deepali Srivastava 19
+>>f3
+>>f3 = f1-2
+
+# Magic Methods - 2
+class Fraction:
+    def __init__(self,nr,dr=1):
+        self.nr = nr
+        self.dr = dr
+        if self.dr < 0:
+            self.nr *= -1
+            self.dr *= -1
+        self._reduce()
+    def show(self):
+        print(f'{self.nr}/{self.dr}')
+    def add(self,other):
+        if isinstance(other,int):
+            other = Fraction(other)
+        f = Fraction(self.nr * other.dr + other.nr * self.dr, self.dr * other.dr)
+        f._reduce()
+        return f
+    def multiply(self,other):
+        if isinstance(other,int):
+            other = Fraction(other)
+        f = Fraction(self.nr * other.nr , self.dr * other.dr)
+        f._reduce()
+        return f
+    def __eq__(self,other):
+        return (self.nr * other.dr) == (self.dr * other.nr)
+    def __lt__(self,other):
+        return (self.nr * other.dr) < (self.dr * other.nr)
+    def __le__(self,other):
+        return (self.nr * other.dr) <= (self.dr * other.nr)
+    def __str__(self):
+        return f'{self.nr}/{self.dr}'
+    def __repr__(self):
+        return f'Fraction({self.nr},{self.dr})'
+    def _reduce(self):
+        h = Fraction.hcf(self.nr, self.dr)
+        if h == 0:
+            return
+        self.nr //= h
+        self.dr //= h
+    
+    @staticmethod
+    def hcf(x,y):
+        x=abs(x)
+        y=abs(y)
+        smaller = y if x>y else x
+        s = smaller
+        while s>0:
+            if x%s==0 and y%s==0:
+                break
+            s-=1
+        return s
+>>>f1 = Fraction(2,3)
+>>>f2 = Fraction(2,3)
+>>>f3 = Fraction(4,6)
+>>>f1 == f2
+>>>f1 == f3
+>>>f1 != f2
+>>>f1 = Fraction(2,3)
+>>>f2 = Fraction(2,3)
+>>>f3 = Fraction(1,5)
+>>>f1 < f2
+>>>f1 <= f2
+>>>f1 < f3
+>>>f3 < f1
+>>>str(f1)
+>>>f1
+>>>f1 = Fraction(3,4)
+>>>f2 = Fraction(4,5)
+>>>f3 = Fraction(1,5)
+>>>L = [f1,f2,f3]
+>>>print(L)
+
+# Magic Methods - 3
+def __radd__(self,other):
+    return self.__add__(other)
+>>f2 = f1+3
+>>f2 = 3 + f1
